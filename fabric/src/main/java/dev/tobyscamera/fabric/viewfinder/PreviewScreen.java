87578cf320414d2bd2465f78a911240fdcbbbde1
toby linas <tobyprime0516@outlook.com>
2026-07-16 08:41:06 +0800

    fix: render full camera preview texture

--- fabric/src/main/java/dev/tobyscamera/fabric/viewfinder/PreviewScreen.java
+++ fabric/src/main/java/dev/tobyscamera/fabric/viewfinder/PreviewScreen.java
@@ -40,7 +40,7 @@ public final class PreviewScreen extends Screen {
         int size = Math.min(width - 40, height - 80);
         int left = (width - size) / 2;
         int top = (height - size - 30) / 2;
-        graphics.blit(textureId, left, top, 0, 0, size, size, frame.image().getWidth(), frame.image().getHeight());
+        graphics.blit(textureId, left, top, size, size, 0.0f, 1.0f, 0.0f, 1.0f);
         graphics.drawCenteredString(font, "%d x %d maps".formatted(frame.gridSize(), frame.gridSize()), width / 2, top + size + 8, 0xFFFFFFFF);
         super.render(graphics, mouseX, mouseY, partialTick);
     }
